734ec51a650cbf42fc484ab9b0f3d24dfac506b1
Ben Abdu <benjaminabdu@live.com>
2023-05-29 14:09:36 +0200

    May 29

---
+++ bootstrap/app.php
@@ -0,0 +1,50 @@
+<?php
+
+    session_start();
+
+    require '../vendor/autoload.php';
+
+    $app =  new \Slim\App([
+        'settings' => [
+            'displayErrorDetails' => true,
+        ],
+    ]);
+
+    $container = $app->getContainer();
+
+    $container['csrf'] = function($c){
+        $guard = new \Slim\Csrf\Guard();
+        $guard->setFailureCallable(function ($request, $response, $next) {
+            
+            die("CSRF Failure, Do Something Here");
+
+            return $next($request, $response);
+        });
+        return $guard;
+    };
+
+    $container['view'] = function ($container) {
+        $view = new \Slim\Views\Twig(__DIR__.'/../views',[
+            'cache' => false
+        ]);
+
+        $basePath = rtrim(str_ireplace('index.php','',$container['request']->getUri()->getBasePath()),'/');
+        $view->addExtension(new Slim\Views\TwigExtension($container['router'],$basePath));
+        $view->addExtension(new App\Middleware\CsrfExtension($container['csrf']));
+
+        return $view;
+    };
+
+    $container['db'] = function(){
+        return new PDO('mysql:host=localhost;dbname=esportsethiopia','root','');
+    };
+
+    $container['flash'] = function(){
+        return new \Slim\Flash\Messages;
+    };
+
+    //$app->add($container->get('csrf'));
+
+    require '../routes/router.php';
+
+?>
